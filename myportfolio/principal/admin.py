1901fb70903036d40a71fa7172907a70a0bc287e
Julio Mendez <12juliomendez12@gmail.com>
2020-06-23 10:52:48 -0400

    Video instalacion tinyMCE

--- myportfolio/principal/admin.py
+++ myportfolio/principal/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Proyecto
+
+from tinymce.widgets import TinyMCE
+from django.db import models
 # Register your models here.
 
-admin.site.register(Proyecto)
+
+class ProyectoAdmin(admin.ModelAdmin):
+
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()}
+    }
+
+admin.site.register(Proyecto, ProyectoAdmin)
